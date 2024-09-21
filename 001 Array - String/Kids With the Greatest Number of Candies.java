b52a9d03941ad2e91321b6ed097d1b26e30e9f55
Vaibhav Sharma <vaibhavmcudc145@gmail.com>
2024-09-21 12:26:31 +0530

    Elite Coding 🤎

--- 001 Array - String/Kids With the Greatest Number of Candies.java
+++ 001 Array - String/Kids With the Greatest Number of Candies.java
@@ -1,19 +1,17 @@
 class Solution {
         public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-            int maximum =0;
-
-        for(int i : candies)
-		    {
-		        if(i>maximum)
-		            maximum=i;
-		    }
-
-        List<Boolean> list = new ArrayList<Boolean>();
-
-        for(int i : candies)
-		  {
-		    list.add(i+extraCandies>=maximum);
-		  }
+		
+            	int maximum =0;
+	        for(int i : candies)
+		{
+			if(i>maximum)
+	                maximum=i;
+		}
+	        List<Boolean> list = new ArrayList<Boolean>();
+	        for(int i : candies)
+		{
+			list.add(i+extraCandies>=maximum);
+		}
 		return list; 
         }
 }
