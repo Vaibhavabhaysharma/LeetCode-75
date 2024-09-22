19971f7bd9104468a137c4046130df7e829327e7
Vaibhav Sharma <vaibhavmcudc145@gmail.com>
2024-09-22 23:38:59 +0530

    Elite Coding 🤎

--- 001 Array - String/Reverse Vowels of a String.java
+++ 001 Array - String/Reverse Vowels of a String.java
@@ -1,18 +1,18 @@
 class Solution {
-    public String reverseVowels(String s) {
-        List<String> vowels = Arrays.asList("a","A","e","E","i","I","o","O","u","U");
+   public static String reverseVowels(String s) {
+		List<String> vowels = Arrays.asList("a","A","e","E","i","I","o","O","u","U");
      	char[] charArray = s.toCharArray();
         int i = 0, j = charArray.length - 1;
 
         while (i < j) {
-            if (vowels.contains(String.valueOf(charArray[i])) && vowels.contains(String.valueOf(charArray[j]))) {
+            if (isVowel(charArray[i]) && isVowel(charArray[j])) {
                 // Swap the vowels
                 char temp = charArray[i];
                 charArray[i] = charArray[j];
                 charArray[j] = temp;
                 i++;
                 j--;
-            } else if (vowels.contains(String.valueOf(charArray[i]))) {
+            } else if (isVowel(charArray[i])) {
                 j--; // Move j back
             } else {
                 i++; // Move i forward
@@ -21,6 +21,16 @@ class Solution {
 
         // Convert the character array back to a string
         s = new String(charArray);
-     	return s;
+     	return s;   
+    }
+
+
+    public static boolean isVowel(char y){
+        if(y == 'a' || y == 'e' || y == 'i' || y == 'o'
+            || y == 'u' || y == 'A' || y == 'E' || y == 'I'
+            || y == 'O' || y == 'U'){
+                return true;
+            }
+        return false;
     }
 }
