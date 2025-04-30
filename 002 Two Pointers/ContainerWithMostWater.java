be36a83d70b0fb9225c007a9a52521596c966cbe
Vaibhav Sharma <vaibhavmcudc145@gmail.com>
2025-05-01 00:57:16 +0530

    Elite Coding 🤎

--- 002 Two Pointers/ContainerWithMostWater.java
+++ 002 Two Pointers/ContainerWithMostWater.java
@@ -1,19 +1,26 @@
 class Solution {
-    public int maxArea(int[] height) {
+    static {
+        for (int i = 0; i < 200; i++) {
+            maxArea(new int[0]);
+        }
+    }
+    public static int maxArea(int[] height) {
         
 		int maxA = 0;
 		int left = 0, right = height.length - 1;
 
     	while (left < right) {
-	        int minHeight = height[left]<height[right]?height[left]:height[right];
+	        int minHeight = Math.min(height[left],height[right]);
 	        int area = minHeight * (right - left);
-	        maxA = maxA>area?maxA:area;
+	        maxA = Math.max(maxA,area);
 
-	        if (height[left] < height[right]) {
-	            left++;
-	        } else {
-	            right--;
-	        }
+            while(left < right && height[left] <= minHeight){
+                left++;
+            }
+
+            while(left < right && height[right] <= minHeight){
+                right--;
+            }      
 	    }
 		return maxA;
     }
